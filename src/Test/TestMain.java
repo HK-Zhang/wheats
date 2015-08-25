fe20e99ccff1a1cdc186c9cc93bec1fbcbfe4b89
HK Zhang <yxzhk@hotmail.com>
2015-08-25 21:24:57 +0800

    setup testing

--- src/Test/TestMain.java
+++ src/Test/TestMain.java
@@ -1,11 +1,15 @@
 package Test;
 import ClusterClique.*;
+import KaggleTube.TrainingProcessor;
 
 public class TestMain {
 	public static void main(String[] args){
 		//Test Case 1
-		ClusterByClique cbc=new ClusterByClique();
-		cbc.Run();
+/*		ClusterByClique cbc=new ClusterByClique();
+		cbc.Run();*/
+		
+		TrainingProcessor dc= new TrainingProcessor();
+		dc.Run();
 		
 		//Test Case 2
 		//ThreadDemo.testMyThreadDemo();
